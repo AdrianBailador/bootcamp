2a3e72f8897de2273da23302287a5cebff8b1bda
Adrian <adrianbailador@hotmail.com>
2021-03-21 17:31:07 +0000

    Rest api

--- part3/index.js
+++ part3/index.js
@@ -2,6 +2,8 @@ const { request, response } = require('express')
 const express = require('express')
 const app = express()
 
+
+app.use(express.json())
 //const http = require('http')
 
 let notes = [
@@ -43,6 +45,52 @@ app.get('/', (request, response) => {
 
 app.get('/api/notes', (request, response)=>{
     response.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response)=>{
+    const id = Number(request.params.id)
+    const note = notes.find(note=>note.id==id)
+    if(note){
+    response.json(note)
+    }else{
+        response.status(404).end()
+    }
+})
+
+
+
+app.post('/api/notes', (request, response) => {
+    const note = request.body
+
+    if(!note || !note.content){
+        return response.status(400).json({
+            error:'note.content is missing'
+        })
+    }
+
+    const ids = notes.map(note => note.id)
+
+const maxId= Math.max(...ids)
+
+    const newNote = {
+        id: maxId + 1,
+        content: note.content,
+        important: typeof note.important != 'undefined' ? note.important : false,
+        date : new Date().toISOString()
+    }
+
+    notes = [...notes, newNote]
+    //notes = notes.concat(newNote) //Otra forma de hacerlo
+
+
+    response.json(newNote)
+})
+
+app.delete('/api/notes/:id', (request,response)=> {
+    const id = Number(request.params.id)
+    notes = notes.filter(note=>note.id != id)
+    response.status(204).end()
+
 })
 /*
 const PORT = 3001
