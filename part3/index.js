6df921404343a39f4b28846b853cb9eca4961bfd
Adrian <adrianbailador@hotmail.com>
2021-03-21 12:50:31 +0000

    Levantar servidor express

--- part3/index.js
+++ part3/index.js
@@ -44,7 +44,12 @@ app.get('/', (request, response) => {
 app.get('/api/notes', (request, response)=>{
     response.json(notes)
 })
-
+/*
 const PORT = 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
+*/
+const PORT = 3001
+app.listen(PORT, ()=> { //El servidor en express es asincrono, cuando termine el servidor de levantarse, ejecutame esto
+    console.log(`Server running on port ${PORT}`)
+})
